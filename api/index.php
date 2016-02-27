8dabb1e3cc6f396030d21689107d4604839c296b
Juan M. Banda <jmbanda@stanford.edu>
2016-02-27 10:23:03 -0800

    first api stub filled
    Added php code for getting conference data. NOTE: Not tested on live DB
    yet.

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
+
+$server_name= "localhost";
+$username = "confer";
+$password = "confer0123";
+$dbname = "myconference";
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
@@ -15,16 +21,35 @@ $app->GET('/', function(Application $app, Request $request) {
 $app->GET($base_path.'/conference', function(Application $app, Request $request) {
 	$id = $request->get('id');    
 	
-	// call mysql 
-	
-	$obj = '';
-	$obj['conference_id'] = $id;
-	$list[] = $obj;
-	
-	$result = '';
-	$result['results'] = $list;
-	$result['status'] = "OK";
-	$output = json_encode($result);
+	$conn = new mysqli($servername, $username, $password, $dbname);
+	$return_arr = array();
+	if ($conn->connect_error) {
+		$return_arr['status'] = "DB_ERROR";
+	} else {
+		$sql = "SELECT * FROM conference_year WHERE conference_year_id=".$id;
+
+		$result_q = $conn->query($sql); 
+		if ($result->num_rows >0 ) {
+			while ($row = $result->fetch_assoc()) {
+			    $row_array['conference_year_id'] = $row['conference_year_id'];
+			    $row_array['full_name'] = $row['full_name'];
+			    $row_array['short_name'] = $row['short_name'];
+			    $row_array['year'] = $row['year'];
+			    $row_array['location'] = $row['location'];
+			    $row_array['start_date'] = $row['start_date'];
+			    $row_array['end_date'] = $row['end_date'];
+			    $row_array['pre_sub_date'] = $row['pre_sub_date'];
+			    $row_array['sub_date'] = $row['sub_date'];	    
+
+			    array_push($return_arr,$row_array);
+			}
+			$return_arr['status'] = "OK";			
+		} else {
+			$return_arr['status'] = "NO RECORDS";
+		}
+	}
+	$conn->close();
+	$output = json_encode($return_arr);
 	return new Response($output);
 });
 
