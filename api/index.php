2ebd38e6b22a310250fb5642c4281a6836acaf5b
Michel Dumontier <michel.dumontier@gmail.com>
2016-02-28 02:48:12 +0000

    added search api

--- api/index.php
+++ api/index.php
@@ -1,35 +1,67 @@
 <?php
 
-$server_name= "localhost";
-$username = "confer";
-$password = "confer0123";
-$dbname = "myconference";
+$opts = new stdClass();
+$opts->servername= "localhost";
+$opts->username = "confer";
+$opts->password = "confer0123";
+$opts->dbname = "myconference";
+$conn = new mysqli($opts->servername, $opts->username, $opts->password, $opts->dbname);
 
 require_once __DIR__ . '/vendor/autoload.php';
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Silex\Application;
-$base_path = "/myconference/api";
+
+# $base_path = "/myconference/api";
+$base_path = "";
+
 $app = new Silex\Application();
-//$app['debug'] = true;
+$app['debug'] = true;
 $app->GET('/', function(Application $app, Request $request) {
-	return new Response("default // redirect");
+	global $conn;
+	$search_term = $request->get('q');
+	$meta = '';
+	$meta['type'] = 'search';
+	$meta['term'] = $search_term;
+
+	$list = array();
+	$sql = "SELECT * FROM conference_year WHERE full_name LIKE \"%$search_term%\"";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0 ) {
+		$a = null;
+		while ($r = $result->fetch_assoc()) {
+			$a['conference_year_id'] = $r['conference_year_id'];
+			$a['full_name']  = $r['full_name'];
+			$a['short_name'] = $r['short_name'];
+			array_push($list,$a);
+		}
+		$response['status'] = "OK";
+	} else {
+		$response['status'] = "NO RECORDS";
+	}
+	$conn->close();
+
+	$meta['num_results'] = $result->num_rows;
+	$response['meta'] = $meta;
+	$response['results'] = $list;
+
+	$json = json_encode($response, JSON_PRETTY_PRINT);
+	return new Response($json);
 });
 
 
 $app->GET($base_path.'/conference', function(Application $app, Request $request) {
+	global $opts;
 	$id = $request->get('id');    
-	
-	$conn = new mysqli($servername, $username, $password, $dbname);
+
+
 	$return_arr = array();
 	if ($conn->connect_error) {
 		$return_arr['status'] = "DB_ERROR";
 	} else {
 		$sql = "SELECT * FROM conference_year WHERE conference_year_id=".$id;
-
-		$result_q = $conn->query($sql); 
-		if ($result->num_rows >0 ) {
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0 ) {
 			while ($row = $result->fetch_assoc()) {
 			    $row_array['conference_year_id'] = $row['conference_year_id'];
 			    $row_array['full_name'] = $row['full_name'];
